ed9d2aa2f47f05131b0915c4210f585716e99ea4
HaroldUCSM <haroldosorio32@gmail.com>
2023-02-19 23:54:07 -0500

    primer commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,29 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: "es2015",
+    polyfillDynamicImport: false,
+    outDir: "build",
+    assetsDir: "assets",
+    minify: true,
+    sourcemap: true,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+  esbuild: {
+    jsxInject: "import React from 'react'",
+    jsxFactory: "React.createElement",
+    jsxFragment: "React.Fragment",
+  },
 });
